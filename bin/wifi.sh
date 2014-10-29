a8e9ed0c2a1ce5ef471afe67e6f66c6436048490
Daisuke FUJITA <dtanshi45@gmail.com>
2014-10-29 23:12:23 +0900

    s/current/name/g

--- bin/wifi.sh
+++ bin/wifi.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-current=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | egrep "agrCtlRSSI|state|lastTxRate| SSID" | cut -d: -f2 | tr "\\n" ";" | sed 's/^[ ]//g' | cut -d";" -f4-4 | cut -d" " -f2-`
+name=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | egrep "agrCtlRSSI|state|lastTxRate| SSID" | cut -d: -f2 | tr "\\n" ";" | sed 's/^[ ]//g' | cut -d";" -f4-4 | cut -d" " -f2-`
 
-if [ "$current" == "" ]
+if [ "$name" == "" ]
 then
     echo "(WIFI off)"
 else
